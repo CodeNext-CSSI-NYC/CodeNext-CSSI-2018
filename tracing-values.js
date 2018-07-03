9aabe9793a6e7d3b0fb660be35a0950347b11c0a
ahob85 <aaron@ahob85.com>
2018-07-03 16:48:42 -0700

    update

--- tracing-values.js
+++ tracing-values.js
@@ -1,1 +1,22 @@
 // Author: FirstName LastName
+var x, y, z;
+x = 10;
+y = x - 4;
+z = x - y + 1;
+x *= 3;
+y = ++z;
+x /= y;
+z %= 4;
+x = (x + y) % 5;
+x = z++;
+y = --z;
+y -= -4;
+y = 4 + 5 * z;
+x = 8 * 7 + 6 / 2;
+y += x - 7;
+z = -x;
+x = x + z + y++;
+x++;
+y -= x;
+z = x - y;
+x -= z;
