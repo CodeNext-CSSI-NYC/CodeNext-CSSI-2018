0f16ab043954b7e28042ddc066caa78386925e0c
Greg Michnikov <greg.michnikov@gmail.com>
2018-07-09 23:38:32 -0400

    Added shortcuts-practice.js

---
+++ shortcuts-practice.js
@@ -0,0 +1,17 @@
+// 1. Decrease the variable below by 1 using two different shortcuts.
+// Print out its value after each step to make sure it worked.
+let a = 7;
+
+// 2. Double the value of the variable below using a multiplication shortcut.
+// Print out its value before and after each step to make sure it worked.
+let b = 18;
+
+// Bonus: Can you think of a way to double the value of a variable using an addition shortcut? Hmm.... (Note: you would never do this, since readability of code is very important and this would be confusing.)
+
+// 3. Predict the value of d. Then print it out to see if you were right.
+let c = 13;
+let d = c++;
+
+// 4. Move the decimal two spots to the left using a shortcut.
+// Print out the value of e before and after to make sure it worked.
+let e = 234.5;
