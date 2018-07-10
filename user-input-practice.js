a63ac068e66397033a52e11bd2d5a3eecc5c73b5
Greg Michnikov <greg.michnikov@gmail.com>
2018-07-09 23:02:14 -0400

    Added user input practice

---
+++ user-input-practice.js
@@ -0,0 +1,10 @@
+// Remember that you can comment parts of your code in and out!
+// Try this as you move from question to question below.
+
+// 1. Ask the user to enter a number, and print out "The square root of ___ is ___ ", but with the blanks filled in correctly. (Not sure how to get the square root of a number in JavaScript? Google it!)
+
+// 2. Ask the user to enter their name, and print out "Your name has ___ letters", but with the blank filled in. Not sure how to get the number of letters in a string in JavaScript? Google it!
+
+// 3. Ask the user to enter any word, and print out "The word ____ starts with the letter ___", but with the blanks filled in. Not sure how to get the first letter of a string in JavaScript? ...
+
+// 4. Ask the user to enter two numbers, and print out "The average of ___ and ___ is ____", but with the blanks filled in.
