c1c5db8d35b10e9a3bc35c519e7b59b936c75947
Greg Michnikov <greg.michnikov@gmail.com>
2018-07-10 22:51:47 -0400

    add logic.js

---
+++ logic.js
@@ -0,0 +1,43 @@
+// Predict what each line will be (true or false) by writing down your answer in the comment
+// Then uncomment the middle lines one at a time to find out if you were right
+
+// 1.
+// true || false
+// Your guess:
+
+// 2.
+// !true || !false
+// Your guess:
+
+// 3.
+// false && !false
+// Your guess:
+
+// 4.
+// (true || false) && (true)
+// Your guess:
+
+// 5.
+// true || (false && true)
+// Your guess:
+
+// 6.
+// (false || !false) && (true || !true)
+// Your guess:
+
+// 7.
+// !(false || !false || !true)
+// Your guess:
+
+// 8.
+// 3 == "3" && 3 === "3"
+// Your guess:
+
+// 9.
+// 12 / 5 == 2
+// Your guess:
+
+// 10.
+// let a = 1;
+// 2 == a++;
+// Your guess:
