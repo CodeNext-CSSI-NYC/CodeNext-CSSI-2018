81dfea0912df6849bca2fb0626f5e0f97df280d3
Greg Michnikov <greg.michnikov@gmail.com>
2018-07-11 10:58:30 -0400

    add functions-practice.js

---
+++ functions-practice.js
@@ -0,0 +1,38 @@
+// MAKE SURE YOU CHOOSE GOOD, RELEVANT NAMES FOR YOUR FUNCTIONS AND PARAMETERS
+
+// 1a. Write (declare) a function that prints out "meow"
+
+// 1b. Call it three times in a row. Run your code to make sure it works.
+
+
+
+// 2a. Write (declare) a function that takes one parameter and prints out the square of that parameter
+
+// 2b. Call it three times with three different parameters. Run your code to make sure it works.
+
+
+
+// 3a. Write (declare) a function that takes two parameters and prints out
+// "_____ is greater than ____" with the blanks filled in correctly with the two inputs.
+
+// 3b. Call it three times with three different parameters
+// Make sure to try cases where the first one is bigger, the second one is bigger, and when they are the same.
+// Run your code to make sure it works.
+
+
+
+// 4a. Write (declare) a function that takes one parameter called name and
+// returns (not prints out, just returns) the string "Hello ____".
+
+// 4b. Call the function and store the result in a variable. Then, print out that variable.
+// Repeat this three times with different names.
+
+
+
+// 5a. Write (declare) a function that takes two parameters and
+// returns (not prints out, just returns) the average of the parameters.
+
+// 5b. Call the function and store the result in a variable. Then, print out that variable
+// in a sentence like "The average is _____".
+// Repeat this three times with different numbers. What happens if you pass in
+// something that isn't a number, like true or "apple"?
