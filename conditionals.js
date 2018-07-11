c319a2b8b7c8bd014c84ba3876663a811d22d5d8
Greg Michnikov <greg.michnikov@gmail.com>
2018-07-10 23:06:58 -0400

    add conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,20 @@
+// Complete these one at a time. Test each one several times to make sure it works.
+// When you are done with each one, comment it out when you move to the next one, so it doesn't keep showing up.
+
+// 1. Ask the user for a number and print whether it is positive or negative or zero.
+
+// 2. Ask the user to enter three numbers, and then print out the biggest number they entered.
+
+// 3. Ask the user for a number and print whether it is odd or even
+
+// 4. Ask the user to enter a letter and tell them if it is a vowel or consonant
+
+// 5. Ask the user to enter a year, and tell them if it was a leap year.
+// (You can use Google if you're not sure when leap years happen.)
+
+// 6. Ask the user if they have been a citizen for at least 9 years (yes or no), ask them their age, and ask if they live in New York state.
+// If they answer yes, their age is above 30, and yes, then tell them they can run for senate in NY. Otherwise, say that they cannot.
+
+// 7. Write a program that asks the user to enter a whole number 1-7 and prints out the corresponding day of the week.
+// If you want another challenge, look up how to use switch statements and use a switch instead of if-else for this.
+// Note that we usually don't bother with switch statements unless there are more than a few elses.
