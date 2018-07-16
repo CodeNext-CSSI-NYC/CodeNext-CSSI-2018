1e47b060558f8bef4a62b221ac68886524b3d378
Greg Michnikov <greg.michnikov@gmail.com>
2018-07-16 14:29:01 -0400

    Add count-hands.js

---
+++ count-hands.js
@@ -0,0 +1,18 @@
+// Copy over your card deck code. You will need that to be working in order
+// to complete this exercise.
+
+// 1. Shuffle the deck. This was explained in memory.js.
+// 2. Deal a "hand" of seven cards to a player.
+// This means there should be a new array called hand that contains 7 cards, and
+// those cards should no longer be in the deck.
+// Print both of them out to make sure.
+// 3. Count how many of each value are in the hand. For example, if the hand ends
+// up with Aceclubs, 2hearts, 2diamonds, 4spades, 5hearts, Jdiamonds, Kclubs ...
+// Your code should print out something like:
+// 1 Ace, 2 2s, 1 4, 1 5, 1 jack and 1 king.
+
+// 4. If you get that working, try doing this with two hands at the same time.
+// 5. If you get that working, create a function called hasTwoPair that takes in
+// an array called hand and returns a boolean indicating whether or not that hand
+// contains exactly two of any value. My example above would return true
+// because it has two 2's. But a hand with 7 different cards would return false.
